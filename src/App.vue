9b907c414f7aa47b1b0772e418d192403d03db10
JonathanFerreira <jonathan.vferreira1@gmail.com>
2017-12-19 00:12:16 -0200

    feat(app): Start app

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <SelectInter v-on:changeI18n="changeI18n"></SelectInter>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import SelectInter from './components/SelectInter'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+// mensagens disponiveis
+const messages = {
+  en: {
+    message: {
+      hello: 'Hello World'
+    }
+  },
+  es: {
+    message: {
+      hello: 'Hola Mundo'
+    }
+  },
+
+  fr: {
+    message: {
+      hello: 'Bonjour le monde'
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages // set locale messages
+})
+
+export default {
+  i18n,
+  name: 'app',
+  components: {
+    HelloWorld,
+    SelectInter
+  },
+  methods: {
+    // método responsavel por mudar a internacionalizaçao do APP
+    changeI18n: function (language) {
+      i18n.locale = language
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SelectInter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <select v-model="selected" v-on:change="emitChangeI18n">
+      <option disabled value="">Escolha um internacionalização</option>
+      <option value="en">Inglês</option>
+      <option value="es">Espanhol</option>
+      <option value="fr">Francês</option>
+    </select>
+    <br>
+    <span>Selecionado: {{ selected }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LocaleHeader',
+    data () {
+      return {
+        selected: ''
+      }
+    },
+    methods: {
+      // metódo que captura a linguagem seleciona e emit
+      // o event para o componente pai
+      emitChangeI18n: function () {
+        this.$emit('changeI18n', this.selected)
+      }
+    }
+  }
+</script>
